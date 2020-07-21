9c025300c52fd650e47a459af4a22e944810f64f
rynproject <rmj634@gmail.com>
2020-07-21 18:28:12 +0700

    nomer 10

---
+++ nomer10.py
@@ -0,0 +1,13 @@
+import json
+orang_dict = [
+  { "id": 1, "name": "Udin", "age": 12 },
+  { "id": 2, "name": "Reane", "age": 51 },
+  { "id": 3, "name": "Budi", "age": 34 },
+  { "id": 4, "name": "Agus", "age": 16 },
+  { "id": 5, "name": "Sari", "age": 19 },
+  { "id": 6, "name": "Ririn", "age": 20 },
+  { "id": 7, "name": "Dessy", "age": 23 }
+]
+
+orang_json = json.dumps(orang_dict)
+file = json.loads(orang_json)
